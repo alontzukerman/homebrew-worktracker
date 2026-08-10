11afe41ea257d29ca178a818b10c73e7ce96a7ed
AlonTzukermanWIX <alontzukerman@users.noreply.github.com>
2026-08-10 15:44:17 +0300

    worktracker 0.6.2

--- Casks/worktracker.rb
+++ Casks/worktracker.rb
@@ -1,6 +1,6 @@
 cask "worktracker" do
-  version "0.6.1"
-  sha256 "a9da698531f6535033c7fa2de35520c8270ad1eecbefd346ed6f89de19dd4bc4"
+  version "0.6.2"
+  sha256 "a7a9f46c4af6669e7d2a36081cdbeeb5505648204f38796733e5a48b07f055dc"
 
   url "https://github.com/alontzukerman/worktracker/releases/download/v#{version}/WorkTracker-#{version}-arm64.dmg"
   name "WorkTracker"
